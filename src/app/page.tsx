17933b82cca4bd7f4cea32b302aa08caeca9d4cb
Sharad Katre <sharadkatre01@gmail.com>
2025-06-29 00:55:43 +0530

    Fixes and Updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
